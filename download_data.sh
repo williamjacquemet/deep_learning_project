5caea7ee81b4553198915f0dfab834fcc0d3d0d1
williamjacquemet <william.jacquemet@protonmail.com>
2019-05-16 21:49:24 +0200

    updating script to download data

--- download_data.sh
+++ download_data.sh
@@ -6,4 +6,6 @@ smbclient -U% //51.15.62.219/sambashare -c "cd Test1/;prompt;mget *"
 cd ../
 mkdir Train1
 smbclient -U% //51.15.62.219/sambashare -c "cd Train1/;prompt;mget *"
-
+cd ../
+mkdir Train
+smbclient -U% //51.15.62.219/sambashare -c "cd Train/;prompt;mget *"

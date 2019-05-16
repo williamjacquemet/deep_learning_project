b92613081b49dc27ad49b84234729cf933564b94
williamjacquemet <william.jacquemet@protonmail.com>
2019-05-16 20:27:26 +0200

    adding download procedure to get data to run the RNN

---
+++ download_data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir Test1
+cd Test1/
+smbclient -U% //51.15.62.219/sambashare -c "cd Test1/;prompt;mget *"
+cd ../
+mkdir Train1
+smbclient -U% //51.15.62.219/sambashare -c "cd Train1/;prompt;mget *"
+

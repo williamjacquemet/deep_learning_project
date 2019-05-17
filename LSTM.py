d435c9285291e51ac5ed6a8b6237951a735aa10f
williamjacquemet <william.jacquemet@protonmail.com>
2019-05-17 20:13:51 +0200

    updating file

--- LSTM.py
+++ LSTM.py
@@ -50,7 +50,7 @@ a = []
 
 for i in range(1,max_range):
     try:
-        file_name = os.path.join('../Train',str(i)+'.wav')
+        file_name = os.path.join('./Train1',str(i)+'.wav')
         X, sr = librosa.load(file_name) 
         mfccs = librosa.feature.mfcc(y=X, sr=sr, n_mfcc=mfcc_value)
         if mfccs.shape[1]!=173:
@@ -73,7 +73,6 @@ with open("train.csv",'r') as files:
             f1.write(line)
 
 train = pd.read_csv(os.path.join( './train.csv'))
-test = pd.read_csv(os.path.join('./test.csv'))
 
 def parser(row):
    ID = row.ID
@@ -89,6 +88,9 @@ with open('y.pickle', 'wb') as handle:
 
 with open('y.pickle','rb') as handle:
      y=pickle.load(handle)
+with open("train.csv",'r') as files:
+    with open("traintest.csv",'w') as f1:
+        n
 with open('a.pickle','rb') as handle:
      a=pickle.load(handle)
 
@@ -142,13 +144,12 @@ opt = SGD(lr=0.001)
 opt1 = keras.optimizers.Adam(lr=0.008, decay=1e-3)
 model = Sequential()
 model.add(CuDNNLSTM(400, input_shape=(100,173), return_sequences=True))
-model.add(Dropout(0.3))
+model.add(Dropout(0.2))
 model.add(CuDNNLSTM(800, return_sequences=False))
 model.add(Dropout(0.5))
 model.add(Dense(800, activation='relu'))
 model.add(Dense(600, activation='relu'))
-model.add(Dense(400, activation='relu'))
-model.add(Dense(400, activation='relu'))
+model.add(Dense(200, activation='relu'))
 model.add(Dense(10, activation='softmax'))
 print(model.summary())
 # 8. Compile model
@@ -156,6 +157,7 @@ model.compile(loss='categorical_crossentropy',
               optimizer=opt1,
               metrics=['accuracy'])
 # 9. Fit model on training data
-model.fit(g, ytrain,batch_size=100, nb_epoch=300, verbose=1, validation_split=0.09)
+model.fit(g, ytrain,batch_size=12, nb_epoch=300, verbose=1, validation_split=0.09)
+
 
 

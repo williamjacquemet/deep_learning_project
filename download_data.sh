86cd1c702427f4f373523da93f4888bf48e7fc9b
williamjacquemet <48022293+williamjacquemet@users.noreply.github.com>
2019-06-09 09:59:13 +0200

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -2,10 +2,10 @@
 
 mkdir Test1
 cd Test1/
-smbclient -U% //51.15.62.219/sambashare -c "cd Test1/;prompt;mget *"
+smbclient -U% //51.158.187.148/sambashare -c "cd Test1/;prompt;mget *"
 cd ../
 mkdir Train1
-smbclient -U% //51.15.62.219/sambashare -c "cd Train1/;prompt;mget *"
+smbclient -U% //51.158.187.148/sambashare -c "cd Train1/;prompt;mget *"
 cd ../
 mkdir Train
-smbclient -U% //51.15.62.219/sambashare -c "cd Train/;prompt;mget *"
+smbclient -U% //51.158.187.148/sambashare -c "cd Train/;prompt;mget *"
